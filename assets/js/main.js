7b4d780722c3cb64e3bf4952a62f25ff5f67c9da
Esteban Cáceres <estebancacerest@gmail.com>
2024-02-14 00:48:50 -0300

    desafio evaluado centro medico pt 2

---
+++ assets/js/main.js
@@ -0,0 +1,313 @@
+document.write("<h1>Estadisticas centro medico ÑUÑOA</h1>");
+
+let radiologia = [
+  {
+    hora: "11:00",
+    especialista: "IGNACIO SCHULZ",
+    paciente: "FRANCISCA ROJAS",
+    rut: "9878782-1",
+    prevision: "FONASA",
+  },
+  {
+    hora: "11:30",
+    especialista: "FEDERICO SUBERCASEAUX",
+    paciente: "PAMELA ESTRADA",
+    rut: "15345241-3",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "15:00",
+    especialista: "FERNANDO WURTHZ",
+    paciente: "ARMANDO LUNA",
+    rut: "16445345-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "15:30",
+    especialista: "ANA MARIA GODOY",
+    paciente: "MANUEL GODOY",
+    rut: "17666419-0",
+    prevision: "FONASA",
+  },
+  {
+    hora: "16:00",
+    especialista: "PATRICIA SUAZO",
+    paciente: "RAMON ULLOA",
+    rut: "14989389-K",
+    prevision: "FONASA",
+  },
+];
+// 2. Eliminar el primer y último elemento del arreglo de Radiología
+radiologia.shift(); // para eliminar el primer elemento
+radiologia.pop(); // para eliminar el segundo elemento
+
+let tabla = "";
+for (let i = 0; i < radiologia.length; i++) {
+  tabla += `
+    <tr>
+    <td>${radiologia[i].hora}</td>
+    <td>${radiologia[i].especialista}</td>
+    <td>${radiologia[i].paciente}</td>
+    <td>${radiologia[i].rut}</td>
+    <td>${radiologia[i].prevision}</td>
+    </tr>
+    `;
+}
+document.getElementById("tablaRadiologia").innerHTML = tabla;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+let traumatologia = [
+  {
+    hora: "8:00",
+    especialista: "MARIA PAZ ALTUZARRA",
+    paciente: "PAULA SANCHEZ",
+    rut: "15554774-5",
+    prevision: "FONASA",
+  },
+  {
+    hora: "10:00",
+    especialista: "RAUL ARAYA",
+    paciente: "ANGÉLICA NAVAS",
+    rut: "15444147-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "10:30",
+    especialista: "MARIA ARRIAGADA",
+    paciente: "ANA KLAPP",
+    rut: "17879423-9",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:00",
+    especialista: "ALEJANDRO BADILLA",
+    paciente: "FELIPE MARDONES",
+    rut: "1547423-6",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:30",
+    especialista: "CECILIA BUDNIK",
+    paciente: "DIEGO MARRE",
+    rut: "16554741-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "12:00",
+    especialista: "ARTURO CAVAGNARO",
+    paciente: "CECILIA MENDEZ",
+    rut: "9747535-8",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "12:30",
+    especialista: "ANDRES KANACRI",
+    paciente: "MARCIAL SUAZO",
+    rut: "11254785-5",
+    prevision: "ISAPRE",
+  },
+];
+// 1. Agregar las siguientes horas al arreglo de Traumatología
+//PUSH AL ARREGLO DE TRAUMATOLOGIA~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\
+
+traumatologia.push(
+  {
+    hora: "09:00",
+    especialista: "RENÉ POBLETE",
+    paciente: "ANA GELLONA",
+    rut: "13123329-7",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "09:30",
+    especialista: "MARIA SOLAR",
+    paciente: "RAMIRO ANDRADE",
+    rut: "12221451-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "10:00",
+    especialista: "RAUL LOYOLA",
+    paciente: "CARMEN ISLA",
+    rut: "10112348-3",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "10:30",
+    especialista: "ANTONIO LARENAS",
+    paciente: "PABLO LOAYZA",
+    rut: "13453234-1",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "12:00",
+    especialista: "MATIAS ARAVENA",
+    paciente: "SUSANA POBLETE",
+    rut: "14345656-6",
+    prevision: "FONASA",
+  }
+);
+
+console.log(traumatologia);
+
+let tabla2 = "";
+for (let i in traumatologia) {
+  tabla2 += `
+    <tr>
+    <td>${traumatologia[i].hora}</td>
+    <td>${traumatologia[i].especialista}</td>
+    <td>${traumatologia[i].paciente}</td>
+    <td>${traumatologia[i].rut}</td>
+    <td>${traumatologia[i].prevision}</td>
+    </tr>
+    `;
+}
+document.getElementById("tablaTraumatologia").innerHTML = tabla2;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+let dental = [
+  {
+    hora: "8:30",
+    especialista: "ANDREA ZUÑIGA",
+    paciente: "MARCELA RETAMAL",
+    rut: "11123425-6",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:00",
+    especialista: "MARIA PIA ZAÑARTU",
+    paciente: "ANGEL MUÑOZ",
+    rut: "9878789-2",
+    prevision: "ISAPRE",
+  },
+  {
+    hora: "11:30",
+    especialista: "SCARLETT WITTING",
+    paciente: "MARIO KAST",
+    rut: "7998789-5",
+    prevision: "FONASA",
+  },
+  {
+    hora: "13:00",
+    especialista: "FRANCISCO VON TEUBER",
+    paciente: "KARIN FERNANDEZ",
+    rut: "18887662-K",
+    prevision: "FONASA",
+  },
+  {
+    hora: "13:30",
+    especialista: "EDUARDO VIÑUELA",
+    paciente: "HUGO SANCHEZ",
+    rut: "17665461-4",
+    prevision: "FONASA",
+  },
+  {
+    hora: "14:00",
+    especialista: "RAQUEL VILLASECA",
+    paciente: "ANA SEPULVEDA",
+    rut: "14441281-0",
+    prevision: "ISAPRE",
+  },
+];
+
+let tabla3 = "";
+for (let i in dental) {
+  tabla3 += `
+    <tr>
+    <td>${dental[i].hora}</td>
+    <td>${dental[i].especialista}</td>
+    <td>${dental[i].paciente}</td>
+    <td>${dental[i].rut}</td>
+    <td>${dental[i].prevision}</td>
+    </tr>
+    `;
+}
+document.getElementById("tablaDental").innerHTML = tabla3;
+
+/* Imprimir en la página HTML, mediante document.write y/o las funciones que estime
+conveniente, la lista de consultas médicas de Dental. Sin embargo, debe hacerlo
+separando por un guión cada dato desplegado y cada fila de información debe estar
+separada por un párrafo */
+document.write(`<h3>Lista de consultas médicas de Dental: </h3>`);
+dental.forEach(function (d) {
+  document.write(
+    `${d.hora} - ${d.especialista} - ${d.paciente} - ${d.rut} - ${d.prevision}<br>`
+  );
+});
+
+/* Imprimir un listado total de todos los pacientes que se atendieron en el centro médico.
+Para esto, deberá unir todos los nombres de pacientes e imprimir uno por cada párrafo */
+
+document.write(`<br/><h3>Lista final de todos los pacientes atentidos: </h3>`);
+
+let todosLosPacientes = radiologia.concat(traumatologia, dental);
+
+todosLosPacientes.forEach(function (paciente) {
+  document.write(`${paciente.paciente}<br>`);
+});
+
+/* Filtrar aquellos pacientes que indican ser de ISAPRE en la lista de consultas médicas
+de Dental */
+
+document.write(
+  `<br/><h3>Lista de pacientes ISAPRE en CONSULTAS DENTALES: </h3>`
+);
+
+let pacientesIsapre = dental.filter(function (cita) {
+  return cita.prevision === "ISAPRE";
+});
+
+pacientesIsapre.forEach(function (paciente) {
+  document.write(`${paciente.paciente} - ${paciente.prevision}<br>`);
+});
+
+/* Filtrar aquellos pacientes que indican ser de FONASA en la lista de consultas médicas
+de Traumatología */
+
+document.write(
+  `<br/><h3>Lista de pacientes FONASA en CONSULTAS de TRAUMATOLOGIA: </h3>`
+);
+
+let pacientesFonasa = traumatologia.filter(function (cita) {
+  return cita.prevision === "FONASA";
+});
+
+pacientesFonasa.forEach(function (paciente) {
+  document.write(`${paciente.paciente} - ${paciente.prevision}<br>`);
+});
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// conetido extra de html ayuda
+
+/* document.write(
+  `<p>Cantidad de atenciones para Radiología: ${radiologia.length}</p>`
+);
+document.write(
+  `<p>Cantidad de atenciones para Traumatología: ${traumatologia.length}</p>`
+);
+document.write(`<p>Cantidad de atenciones para Dental: ${dental.length}</p>`);
+
+document.write(
+  `<p>Primera atencion: ${radiologia[0].paciente} - ${
+    radiologia[0].prevision
+  } | Última atención: ${radiologia[radiologia.length - 1].paciente} - ${
+    radiologia[radiologia.length - 1].prevision
+  }.</p>`
+);
+document.write(
+  `<p>Primera atencion: ${traumatologia[0].paciente} - ${
+    traumatologia[0].prevision
+  } | Última atención: ${traumatologia[traumatologia.length - 1].paciente} - ${
+    traumatologia[traumatologia.length - 1].prevision
+  }.</p>`
+);
+document.write(
+  `<p>Primera atencion: ${dental[0].paciente} - ${
+    dental[0].prevision
+  } | Última atención: ${dental[dental.length - 1].paciente} - ${
+    dental[dental.length - 1].prevision
+  }.</p>`
+);
+ */
